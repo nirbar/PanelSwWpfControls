e940f2c0f03120dff31303044ccfd08a87500f8e
nir.bar <nir.bar@panel-sw.com>
2019-10-02 16:32:26 +0300

    VisualState for WaitableButton

--- PanelSwWpfControls/WaitableButton.cs
+++ PanelSwWpfControls/WaitableButton.cs
@@ -3,11 +3,22 @@ using System.Windows.Controls;
 
 namespace PanelSW.WPF.Controls
 {
+    [TemplateVisualState(Name = "Waiting", GroupName = "ValueStates")]
+    [TemplateVisualState(Name = "Ready", GroupName = "ValueStates")]
     public class WaitableButton : Button
     {
         #region IsWaiting
 
-        public static readonly DependencyProperty IsWaitingProperty = DependencyProperty.Register("IsWaiting", typeof(bool), typeof(WaitableButton), new PropertyMetadata(false));
+        public static readonly DependencyProperty IsWaitingProperty = DependencyProperty.Register("IsWaiting", typeof(bool), typeof(WaitableButton), new PropertyMetadata(false, OnIsWaitingChanged));
+        private static void OnIsWaitingChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            WaitableButton me = d as WaitableButton;
+            if (me != null)
+            {
+                VisualStateManager.GoToState(me, me.IsWaiting ? "Waiting" : "Ready", true);
+            }
+        }
+
         public bool IsWaiting
         {
             get
