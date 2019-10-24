6f26281611ef316cbe9ed74bc74ab585c1a96358
Nir Bar <nir.bar@panel-sw.co.il>
2019-10-24 16:53:44 +0300

    - WaitableButton:   - Bind command   - Add keyed style for blinking foreground - EyePasswordBox:   - Keyed border style

--- PanelSwWpfControls/ResourceKeys.cs
+++ PanelSwWpfControls/ResourceKeys.cs
@@ -7,9 +7,11 @@ namespace PanelSW.WPF.Controls
         public static readonly ComponentResourceKey WaitableButtonProgressBarStyleKey = new ComponentResourceKey(typeof(ResourceKeys), "WaitableButtonProgressBarStyle");
         public static readonly ComponentResourceKey WaitableButtonButtonStyleKey = new ComponentResourceKey(typeof(ResourceKeys), "WaitableButtonButtonStyle");
         public static readonly ComponentResourceKey WaitableButtonBorderStyleKey = new ComponentResourceKey(typeof(ResourceKeys), "WaitableButtonBorderStyle");
+        public static readonly ComponentResourceKey WaitableButtonBlinkingStyleKey = new ComponentResourceKey(typeof(ResourceKeys), "WaitableButtonBlinkingStyle");
 
         public static readonly ComponentResourceKey EyePasswordBoxWatermarkPasswordBoxStyleKey = new ComponentResourceKey(typeof(ResourceKeys), "EyePasswordBoxWatermarkPasswordBoxStyle");
         public static readonly ComponentResourceKey EyePasswordBoxTextBoxStyleKey = new ComponentResourceKey(typeof(ResourceKeys), "EyePasswordBoxTextBoxStyle");
         public static readonly ComponentResourceKey EyePasswordBoxButtonStyleKey = new ComponentResourceKey(typeof(ResourceKeys), "EyePasswordBoxButtonStyle");
+        public static readonly ComponentResourceKey EyePasswordBoxBorderStyleKey = new ComponentResourceKey(typeof(ResourceKeys), "EyePasswordBoxBorderStyle");
     }
 }
